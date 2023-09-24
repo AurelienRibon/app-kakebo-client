2f61b9f3e199f31f6f9656411c05d19de2db2a07
Aurelien Ribon <aurelien.ribon@gmail.com>
2023-09-24 11:47:23 +0200

    Use modern JS private

--- src/store/store.ts
+++ src/store/store.ts
@@ -8,32 +8,32 @@ import { Expense, ExpenseSpec } from '../models/expense';
 type DbExpensesSyncResult = { expenses: ExpenseJSON[] };
 
 class Store {
-  private _expensesFull = ref([]) as Ref<Expense[]>;
-  private _expenses = ref([]) as Ref<Expense[]>;
-  private _loading = ref(false);
-  private _version = 1;
+  #expensesAll = ref([]) as Ref<Expense[]>;
+  #expenses = ref([]) as Ref<Expense[]>;
+  #loading = ref(false);
+  #version = 1;
 
   get expenses(): Ref<Expense[]> {
-    return this._expenses;
+    return this.#expenses;
   }
 
   get loading(): Ref<boolean> {
-    return this._loading;
+    return this.#loading;
   }
 
   get version(): number {
-    return this._version;
+    return this.#version;
   }
 
   addExpense(spec: ExpenseSpec): void {
     const expense = new Expense(spec);
-    this._expensesFull.value.push(expense);
+    this.#expensesAll.value.push(expense);
     this.refreshExpenses();
   }
 
   refreshExpenses(): void {
-    this._expenses.value = this._expensesFull.value.filter((it) => !it.deleted);
-    sortExpenses(this._expenses.value);
+    this.#expenses.value = this.#expensesAll.value.filter((it) => !it.deleted);
+    sortExpenses(this.#expenses.value);
   }
 
   async loadAndSync(): Promise<void> {
@@ -53,17 +53,17 @@ class Store {
       const { value } = await Preferences.get({ key: 'data' });
       const spec = value ? JSON.parse(value) : { expenses: [] };
       const specExpenses = spec.expenses as ExpenseJSON[];
-      this._setExpenses(createExpensesFromJSONs(specExpenses));
+      this.#setExpenses(createExpensesFromJSONs(specExpenses));
     } catch (err) {
       logError(err);
-      this._setExpenses([]);
+      this.#setExpenses([]);
     }
   }
 
   async save(): Promise<void> {
     const value = JSON.stringify({
-      expenses: this._expensesFull.value.map((it) => it.serialize()),
-      version: this._version,
+      expenses: this.#expensesAll.value.map((it) => it.serialize()),
+      version: this.#version,
     });
 
     await Preferences.set({ key: 'data', value });
@@ -72,10 +72,10 @@ class Store {
   async sync() {
     logInfo('Synchronizing expenses...');
 
-    this._loading.value = true;
+    this.#loading.value = true;
 
     try {
-      const changed = await this._syncExpenses();
+      const changed = await this.#syncExpenses();
       if (changed) {
         await this.save();
       }
@@ -83,14 +83,14 @@ class Store {
       logError(err);
     }
 
-    this._loading.value = false;
+    this.#loading.value = false;
   }
 
   // Internal API
   // ---------------------------------------------------------------------------
 
-  private async _syncExpenses() {
-    const syncResult = await syncExpensesWithDB(this._expensesFull.value);
+  async #syncExpenses() {
+    const syncResult = await this.#postSync(this.#expensesAll.value);
     if (!syncResult) {
       return;
     }
@@ -98,17 +98,17 @@ class Store {
     let changed = false;
 
     if (syncResult.expenses.length > 0) {
-      this._upsertExpenseJSONs(syncResult.expenses);
+      this.#upsertExpenseJSONs(syncResult.expenses);
       changed = true;
     }
 
     return changed;
   }
 
-  private _upsertExpenseJSONs(jsons: ExpenseJSON[]): void {
+  #upsertExpenseJSONs(jsons: ExpenseJSON[]): void {
     logInfo('Creating expenses from jsons...');
 
-    const knownExpensesById = new Map(this._expensesFull.value.map((it) => [it.id, it]));
+    const knownExpensesById = new Map(this.#expensesAll.value.map((it) => [it.id, it]));
 
     for (const json of jsons) {
       try {
@@ -119,43 +119,42 @@ class Store {
       }
     }
 
-    this._setExpenses(Array.from(knownExpensesById.values()));
+    this.#setExpenses(Array.from(knownExpensesById.values()));
   }
 
-  private _setExpenses(expenses: Expense[]): void {
-    this._expensesFull.value = expenses;
+  #setExpenses(expenses: Expense[]): void {
+    this.#expensesAll.value = expenses;
     this.refreshExpenses();
+  }
+
+  // Server requests
+  // ---------------------------------------------------------------------------
+
+  async #postSync(expenses: Expense[]): Promise<DbExpensesSyncResult | undefined> {
+    const body = JSON.stringify({
+      route: '/expenses/sync',
+      expenses: expenses.map((it) => it.serialize()),
+    });
+
+    const headers = { 'Content-Type': 'application/json' };
+    const prod = process.env.NODE_ENV === 'production';
+    const url = prod
+      ? 'https://kakebo.aurelienribon.repl.co/expenses/sync'
+      : 'https://kakebo.aurelienribon.repl.co/expenses/sync?dev=1';
+
+    logInfo('Fetching remote...');
+    const fn = () => fetch(url, { method: 'POST', headers, body });
+    const res = await retry(fn);
+
+    if (!res.ok) {
+      logInfo(`Request failed with status ${res.status} (${res.statusText}).`);
+      logInfo(await res.text());
+      return;
+    }
+
+    logInfo('Fetch success!');
+    return res.json();
   }
 }
 
 export const store = new Store();
-
-// -----------------------------------------------------------------------------
-// DB SYNC
-// -----------------------------------------------------------------------------
-
-async function syncExpensesWithDB(expenses: Expense[]): Promise<DbExpensesSyncResult | undefined> {
-  const body = JSON.stringify({
-    route: '/expenses/sync',
-    expenses: expenses.map((it) => it.serialize()),
-  });
-
-  const headers = { 'Content-Type': 'application/json' };
-  const prod = process.env.NODE_ENV === 'production';
-  const url = prod
-    ? 'https://kakebo.aurelienribon.repl.co/expenses/sync'
-    : 'https://kakebo.aurelienribon.repl.co/expenses/sync?dev';
-
-  logInfo('Fetching remote...');
-  const fn = () => fetch(url, { method: 'POST', headers, body });
-  const res = await retry(fn);
-
-  if (!res.ok) {
-    logInfo(`Request failed with status ${res.status} (${res.statusText}).`);
-    logInfo(await res.text());
-    return;
-  }
-
-  logInfo('Fetch success!');
-  return res.json();
-}
