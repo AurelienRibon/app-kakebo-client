772572266c1154532d64e74255c7d73a3f026e61
Aurelien Ribon <aurelien.ribon@gmail.com>
2023-09-27 14:45:02 +0200

    Update splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.aurelienribon.kakebo',
   appName: 'Kakebo',
   webDir: 'dist',
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 0,
-    },
-  },
 };
 
 export default config;
